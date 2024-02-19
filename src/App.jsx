7ff3d6a33c87dea5065842e4103b27a9d603b186
Nikolai Andrei Calizar <tkshclz07@gmail.com>
2024-02-19 20:39:16 +0800

    Adding the N-Word.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+// Import Bootstrap Styles
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  return <p>Test App</p>;
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <h1
+        className="text-center display-4 display-md-3 display-lg-2"
+        style={{ fontStyle: "bold" }}
+      >
+        <strong>NIGGER</strong>
+      </h1>
+    </div>
+  );
 }
 
 export default App;
