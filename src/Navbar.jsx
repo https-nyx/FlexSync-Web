f988929c9da8ff3ddec9105f80210d5925270d36
K1-aD <153174317+K1-aD@users.noreply.github.com>
2024-02-22 15:22:55 +0800

    Add files via upload

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (  
+     <nav className="navbar">
+        <h1>FlexSync</h1>
+        <div className="Links">
+            <a href="/">Home</a>
+            <a href="/create" style={{
+
+            }}>Profile</a>
+            <a href="/">About Us</a>
+            <a href="/">Contact</a>
+        </div>
+     </nav>
+    );
+}
+ 
+export default Navbar; 
